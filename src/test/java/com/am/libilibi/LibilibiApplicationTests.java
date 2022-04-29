662c0e15daa2eacfd05bbe416c8aabd40b5be21e
arturiamumu <arturiamumu@gmail.com>
2022-04-29 14:50:03 +0800

    base springboot frame

---
+++ src/test/java/com/am/libilibi/LibilibiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am.libilibi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibilibiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
