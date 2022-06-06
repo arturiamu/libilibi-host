a36bded6bed204bc1ad2b42bfb74a5c881b27178
orange_puupy <1589691384@qq.com>
2022-06-06 13:26:25 +0800

    lyy

---
+++ src/test/java/com/am/email/EmailTests.java
@@ -0,0 +1,20 @@
+package com.am.email;
+
+import com.am.adastra.util.EmailUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: lyy
+ * @Version: 1.0
+ * @JDK-version: 1.8
+ * @Date: 2022/6/6 13:20
+ * @Description:
+ */
+public class EmailTests {
+
+    @Test
+    public void send(){
+        EmailUtil.sendEmail("test","test");
+    }
+}
