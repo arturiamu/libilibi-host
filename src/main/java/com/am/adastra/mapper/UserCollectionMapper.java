03c3afd2ea89c72bba7d68bda9a089173d77afcf
student <2761804524@qq.com>
2022-06-07 15:08:16 +0800

    zlw2

--- src/main/java/com/am/adastra/mapper/UserCollectionMapper.java
+++ src/main/java/com/am/adastra/mapper/UserCollectionMapper.java
@@ -11,13 +11,8 @@ import java.util.List;
 @Mapper
 public interface UserCollectionMapper {
     /*
-     * 通过视频id和用户id   在收藏夹中查找   判断该用户是否收藏了这个视频
+     * 通过视频id和用户id   在收藏夹中查找判断该用户是否收藏了这个视频
      * */
-    @Select("select * from collection where user_id=#{userId} and collection_video_id=#{collectionVideoId}")
-    //这里的名称和数据库中的名称不一样，因此我们需要利用Result来声明不一样的变量
-    @Result(property = "collectionVideoId", column = "collection_video_id")
-    @Result(property = "collectionTime", column = "collection_time")
-    @Result(property = "userId", column = "user_id")
     UserCollection selectByUserId(Integer userId, Integer collectionVideoId);
 
 
