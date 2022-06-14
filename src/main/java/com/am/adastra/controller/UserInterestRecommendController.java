457d7cabb12f8a46e9b616b2cdc779d8fcbbdb5c
student <2761804524@qq.com>
2022-06-14 09:50:27 +0800

    优化历史记录

--- src/main/java/com/am/adastra/controller/UserInterestRecommendController.java
+++ src/main/java/com/am/adastra/controller/UserInterestRecommendController.java
@@ -1,10 +1,12 @@
 package com.am.adastra.controller;
 
 import com.am.adastra.entity.User;
+import com.am.adastra.service.UserInterestRecommendService;
 import com.am.adastra.service.UserService;
 import com.am.adastra.util.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,16 +19,22 @@ import javax.servlet.http.HttpServletRequest;
 @Slf4j
 @RestController
 @RequestMapping("/interest")
-public class UserInterestRecommend {
+public class UserInterestRecommendController {
     @Autowired
     private UserService userService;
+    @Qualifier("userInterestRecommendServiceImpl")
+    @Autowired(required = false)
+    private UserInterestRecommendService interestService;
 
     @GetMapping
     public Result<Void> interest(HttpServletRequest request) {
         //  获取当前用户的用户 id
         User user = userService.isLogin(request.getSession());
-        Integer userId = user.getId();
+        long userId = user.getId();
         log.info("当前的用户id为" + userId);
+
+        //2.调用service层方法获取推荐
+        interestService.list(userId);
 
         return null;
 
