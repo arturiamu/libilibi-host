98ff5c2ac1ccc3a3e5a1abc1c5f85b24df7c8bc3
arturiamumu <arturiamumu@gmail.com>
2022-06-24 21:32:03 +0800

    avatar base

--- src/main/java/com/am/adastra/controller/UserAvatarController.java
+++ src/main/java/com/am/adastra/controller/UserAvatarController.java
@@ -1,10 +1,14 @@
 package com.am.adastra.controller;
 
+import com.am.adastra.entity.User;
+import com.am.adastra.ex.UserNotLoginException;
 import com.am.adastra.service.UserAvatarService;
+import com.am.adastra.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @Author : ArturiaMu KMUST-Stu
@@ -19,4 +23,16 @@ import javax.annotation.Resource;
 public class UserAvatarController {
     @Resource
     UserAvatarService userAvatarService;
+    @Resource
+    UserService userService;
+
+
+
+    public void getAvatar(HttpServletRequest request) {
+        if (userService.isLogin(request.getSession()) == null) {
+            throw new UserNotLoginException("用户未登录");
+        }
+        User sessionUser = (User) request.getSession().getAttribute(UserController.USER_INFO_SESSION);
+        userAvatarService.getByUid(sessionUser.getId());
+    }
 }
