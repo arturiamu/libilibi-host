7eb0a2e4bf7339f254e4d18d4951d38942dbd6fe
arturiamumu <arturiamumu@gmail.com>
2022-07-01 10:28:50 +0800

    version 1 release

--- src/main/java/com/am/adastra/controller/Admin/AdminController.java
+++ src/main/java/com/am/adastra/controller/Admin/AdminController.java
@@ -2,17 +2,11 @@ package com.am.adastra.controller.Admin;
 
 
 
-import cn.hutool.poi.excel.ExcelUtil;
-import cn.hutool.poi.excel.ExcelWriter;
 import com.am.adastra.entity.Admin;
-import com.am.adastra.entity.User;
-import com.am.adastra.entity.UserDBO;
 import com.am.adastra.entity.dto.AdminDTO;
 import com.am.adastra.entity.vo.AdminVO;
-import com.am.adastra.entity.vo.UserVO;
 import com.am.adastra.service.AdminService;
 import com.am.adastra.service.UserService;
-import com.am.adastra.util.POJOUtils;
 import com.am.adastra.util.Result;
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import io.swagger.annotations.Api;
@@ -23,14 +17,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 /*
@@ -41,8 +29,7 @@ import java.util.Objects;
 @Slf4j
 @Api(tags = "管理员模块")
 public class AdminController {
-    public static final String USER_INFO_SESSION = "userInfoSession";
-//    public static final String SESSION_NAME = "userInfo";
+    public static final String ADMIN_INFO_SESSION = "adminInfoSession";
     @Autowired
     UserService userService;
     @Autowired
@@ -68,7 +55,7 @@ public class AdminController {
         BeanUtils.copyProperties(getAdmin,admin);
         result.setSuccess(getAdmin);
 
-        request.getSession().setAttribute(USER_INFO_SESSION, admin);
+        request.getSession().setAttribute(ADMIN_INFO_SESSION, admin);
 
         return result;
     }
@@ -97,7 +84,7 @@ public class AdminController {
         log.info("用户退出登录");
         Result<Void> result = new Result<>();
         result.setSuccess("用户退出登录成功！", null);
-        request.getSession().setAttribute(USER_INFO_SESSION, null);
+        request.getSession().setAttribute(ADMIN_INFO_SESSION, null);
         return result;
     }
 
