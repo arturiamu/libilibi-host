66f76d0afd6a086f791666bb162d3fa2f71fd69f
arturiamumu <arturiamumu@gmail.com>
2022-07-01 17:47:57 +0800

    version 1 release local

--- src/main/java/com/am/adastra/app/CachePreLoad.java
+++ src/main/java/com/am/adastra/app/CachePreLoad.java
@@ -51,14 +51,15 @@ public class CachePreLoad implements ApplicationRunner {
         public void run() {
             while (true) {
                 log.warn("start load cache");
-                log.info("准备执行收藏夹缓存预热....");
+                log.warn("准备执行收藏夹缓存预热....");
                 that.userCollectionService.preloadCache();
-                log.info("收藏夹缓存预热执行完成....");
-                log.info("准备执行历史记录缓存预热....");
+                log.warn("收藏夹缓存预热执行完成....");
+                log.warn("准备执行历史记录缓存预热....");
                 that.userHistoryService.preloadCache();
-                log.info("历史记录缓存预热执行完成....");
+                log.warn("历史记录缓存预热执行完成....");
                 log.warn("end load cache");
                 //线程睡眠10分钟然后重新获取用户的爱好内容
+                log.warn("11111111111111111111111111111111111111111111111111111111111111111");
                 try {
                     Thread.sleep(1 * 1000 * 60 * 10);
                 } catch (InterruptedException ignored) {
